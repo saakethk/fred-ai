4c58fa541838dbc1ae6b98ef6cef79d6f15c49fe
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2025-07-22 21:09:48 -0400

    Following features added: Tweet creation, Secrets integration, Firestore integration, Document based run trigger for functions

--- functions/main.py
+++ functions/main.py
@@ -9,17 +9,24 @@ from google import genai
 import requests
 import google
 import json
+from requests_oauthlib import OAuth1Session
+import os
+import json
+from dotenv import load_dotenv
 
-# Initializes firebase app
+# Initializes firebase app and dotenv
+load_dotenv() # This loads variables from the .env file
 initialize_app()
 
 # BACKEND FUNCTIONS
 
 # Retrieves secret keys
-client = genai.Client(api_key="AIzaSyAkl3rImltcSRA3FzcBSZuVkpDZ64t6BKg")
-news_api_key = "Jzmw6TMctEZN073SFitfu5cQEIQfwviZZjews9ZD"
-stocks_api_key = "d10s3c9r01qse6ldmv0gd10s3c9r01qse6ldmv10"
-market_api_keys = ("PKI5WDQQCSEKVAL3MYRP", "BGId9iziU8kQ54JqloXe3zbxasLbEHt5YgP0I8AC")
+client = genai.Client(api_key=os.getenv("GOOGLE_GENAI_API_KEY"))
+news_api_key = os.getenv("NEWS_API_KEY")
+stocks_api_key = os.getenv("STOCKS_API_KEY")
+market_api_keys = (os.getenv("MARKET_API_KEY"), os.getenv("MARKET_API_SECRET"))
+twitter_api_keys = (os.getenv("TWITTER_API_KEY"), os.getenv("TWITTER_API_SECRET"))
+twitter_access_tokens = (os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
 
 # Gets timestamp in accesible format
 def get_timestamp(with_date=False, delta=1) -> str:
@@ -89,7 +96,7 @@ def get_stock_updates(symbol: str, name: str):
             # Gets stock price at time
             status, stock_price_res = get_stock_price()
             if status == True:
-                parsed_response["price"] = get_stock_price()
+                parsed_response["price"] = stock_price_res
                 return True, parsed_response
             else:
                 return False, stock_price_res
@@ -135,7 +142,7 @@ def index_stock(req: https_fn.Request) -> https_fn.Response:
 
 # Updates stock entry on recurring basis
 @https_fn.on_request()
-def update_stocks() -> https_fn.Response:
+def update_stocks(req: https_fn.Request) -> https_fn.Response:
 
     # Gets stocks in collection
     firestore_client: google.cloud.firestore.Client = firestore.client()
@@ -148,12 +155,16 @@ def update_stocks() -> https_fn.Response:
         status, res = get_stock_updates(symbol=stock_data["symbol"], name=stock_data["name"])
         if status == True:
 
+            if "live_stance" not in stock_data:
+                stock_data["live_stance"] = "neutral"
+
             # Create stock update
             _, update_ref = firestore_client.collection("updates").add(
                 {
                     "symbol": stock_data["symbol"],
                     "name": stock_data["name"],
                     "summary": res["summary"],
+                    "prev_stance": stock_data["live_stance"],
                     "stance": res["stance"] if (res["stance"] == "bearish" or res["stance"] == "bullish") else "neutral",
                     "defense": res["defense"],
                     "sources": res["sources"],
@@ -176,7 +187,7 @@ def update_stocks() -> https_fn.Response:
 
 # Runs update stock function when market conditions satisfied
 @scheduler_fn.on_schedule(schedule="0 */1 * * *")
-def update_stocks_auto() -> https_fn.Response:
+def update_stocks_auto(event: scheduler_fn.ScheduledEvent) -> https_fn.Response:
 
     # Gets market status when run
     params = {
@@ -300,20 +311,70 @@ def post_data_alpaca(url: str, payload: dict) -> tuple[bool, dict | str]:
 
 # Utilizes post sentiments about stocks to paper trade
 @on_document_created(document="updates/{updateId}")
-def paper_trade(event: Event[DocumentSnapshot]):
+def paper_trade(event: Event[DocumentSnapshot]) -> None:
+
+    # Makes update readable
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+    update = event.data.to_dict()
+
+    # Sells stock if not already owned
+    def sell_stock(symbol: str, amount: int) -> None:
+        # Sells stock
+        payload = {
+            "type": "market",
+            "time_in_force": "day",
+            "symbol": symbol,
+            "notional": amount,
+            "side": "sell"
+        }
+        status, buy_stock_res = post_data_alpaca(url="v2/orders", payload=payload)
+        if status == True:
+            print("Stock bought successfully")
+            if "associated_actions" not in update:
+                update["assocaited_actions"] = []
+            assoc_action = {
+                "type": "order",
+                "action": "sell",
+                "alpaca_order_id": buy_stock_res["id"],
+                "timestamp": firestore.SERVER_TIMESTAMP
+            }
+            update["associated_actions"].append(assoc_action)
+            update_ref = firestore_client.collection("updates").document(event.data.id)
+            update_ref.set(update)
+            order_ref = firestore_client.collection("orders")
+            order_ref.add(assoc_action)
+        else:
+            print(buy_stock_res)
 
     # Liquadates positions in case of bearish signal
     def liquadate_position(symbol: str, percent: int) -> None:
         # Gets open position of symbol
         status, open_pos_res = get_data_alpaca(url=f"v2/positions/{symbol}")
         print(open_pos_res)
-        if status == True:
+        if (status == True):
             # Sells stock
             status, sell_request = del_data_alpaca(url=f"v2/positions/{symbol}?percentage={percent}")
             if status == True:
                 print("Stock sold successfully")
+                if "associated_actions" not in update:
+                    update["assocaited_actions"] = []
+                assoc_action = {
+                    "type": "order",
+                    "action": "sell",
+                    "alpaca_order_id": sell_request["id"],
+                    "timestamp": firestore.SERVER_TIMESTAMP
+                }
+                update["associated_actions"].append(assoc_action)
+                update_ref = firestore_client.collection("updates").document(event.data.id)
+                update_ref.set(update)
+                order_ref = firestore_client.collection("orders")
+                order_ref.create(assoc_action)
             else:
                 print(sell_request)
+        else:
+            print("No open position found for symbol:", symbol)
+            # If no open position, sells shorts on stock
+            sell_stock(symbol=symbol, amount=100)
 
     # Buys stock in case of bullish signal
     def buy_stock(symbol: str, amount: int) -> None:
@@ -332,6 +393,19 @@ def paper_trade(event: Event[DocumentSnapshot]):
                 status, buy_stock_res = post_data_alpaca(url="v2/orders", payload=payload)
                 if status == True:
                     print("Stock bought successfully")
+                    if "associated_actions" not in update:
+                        update["assocaited_actions"] = []
+                    assoc_action = {
+                        "type": "order",
+                        "action": "sell",
+                        "alpaca_order_id": buy_stock_res["id"],
+                        "timestamp": firestore.SERVER_TIMESTAMP
+                    }
+                    update["associated_actions"].append(assoc_action)
+                    update_ref = firestore_client.collection("updates").document(event.data.id)
+                    update_ref.set(update)
+                    order_ref = firestore_client.collection("orders")
+                    order_ref.add(assoc_action)
                 else:
                     print(buy_stock_res)
             else:
@@ -340,10 +414,62 @@ def paper_trade(event: Event[DocumentSnapshot]):
             print(account_res)
         
     # Reads update and runs correct function
-    update = event.data.to_dict()
     symbol, signal = update["symbol"], update["stance"]
     if signal == "bullish":
         buy_stock(symbol=symbol, amount=100)
     elif signal == "bearish":
-        liquadate_position(symbol=symbol, percent=100)
+        liquadate_position(symbol=symbol, percent=100, bypass_ownership_check=True)
+
+
+# Utilizes post sentiments about stocks to post to twitter
+@on_document_created(document="updates/{updateId}")
+def create_tweet(event: Event[DocumentSnapshot]) -> None:
+
+    # Makes update readable
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+    update = event.data.to_dict()
+
+    # Summarizes summary even further via AI
+    summary = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Summarize the following summary of stock news into an objective, engaging 240 character tweet. The word limit is very strict and cannot go over 240 characters but can be below. Summary: {update['summary']}",
+    )
+
+    # Defines tweet body
+    poll = {
+        "options": ["Bearish", "Bullish", "Neutral"],
+        "duration_minutes": 60 * 24
+    }
+    payload = {
+        "text": f"{summary.text}\nHow does this news make you feel?",
+        "poll": poll
+    }
+
+    # Make the request
+    oauth = OAuth1Session(
+        twitter_api_keys[0],
+        client_secret=twitter_api_keys[1],
+        resource_owner_key=twitter_access_tokens[0],
+        resource_owner_secret=twitter_access_tokens[1],
+    )
+
+    # Making the request
+    response = oauth.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+    )
+
+    if response.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+
+    print("Response code: {}".format(response.status_code))
+
+    # Parses response for tweet id
+    tweet_id = response.json()["data"]["id"]
+    update["associated_tweet_id"] = tweet_id
+    update["associated_tweet_summary"] = summary.text
+    update_ref = firestore_client.collection("updates").document(event.data.id)
+    update_ref.set(update)
 
